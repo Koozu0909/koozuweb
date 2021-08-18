0ea2b220ad6816c0c3e0eba914c46429caeafb0f
Koozu0909 <87105119+Koozu0909@users.noreply.github.com>
2021-08-18 20:53:17 +0700

    init

---
+++ login&registered/login.js
@@ -0,0 +1,6 @@
+function  toggleFrom(){
+    container=document.querySelector(".container");
+    container.classList.toggle("active");
+    main=document.querySelector(".login__registered");
+    main.classList.toggle("active");
+}
